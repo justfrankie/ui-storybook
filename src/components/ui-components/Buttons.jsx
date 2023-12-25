8e652f1a591fe5d37d389cb2f35a2e1d510c3bed
justfrankie <zhuangfrankie@gmail.com>
2023-12-25 14:58:42 +0800

    add accent color

--- src/components/ui-components/Buttons.jsx
+++ src/components/ui-components/Buttons.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import buttonStyles from "./Buttons.module.css";
 
 const Buttons = () => {
   return (
     <div>
-      <button>Style 1</button>
-      <button>Style 2</button>
-      <button>Style 3</button>
+      <button className={`${buttonStyles.styleOne} ${buttonStyles.primary}`}>
+        Style 1
+      </button>
+      <button className={`${buttonStyles.styleOne} ${buttonStyles.secondary}`}>
+        Style 2
+      </button>
+      <button className={`${buttonStyles.styleOne} ${buttonStyles.accent}`}>
+        Style 3
+      </button>
     </div>
   );
 };
