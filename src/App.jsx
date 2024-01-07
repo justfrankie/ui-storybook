01978faa0cf50aeff786e8dd1070e8b727acdc37
justfrankie <zhuangfrankie@gmail.com>
2024-01-07 17:59:52 +0800

    react router implementation

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,22 @@ import MainWindow from "./components/layout/MainWindow.jsx";
 import SideNavigation from "./components/layout/SideNavigation.jsx";
 import TopNavigationBar from "./components/layout/TopNavigationBar.jsx";
 import componentsList from "./componentsList.jsx";
-import "./App.css";
-
-const styles = {
-  mainContainer: {
-    width: "100%",
-    height: "100vh",
-  },
-  topNavContainer: {
-    width: "100%",
-  },
-  bottomContainer: {
-    display: "flex",
-  },
-  sideNav: {
-    backgroundColor: "rgb(165 195 239 / 14%)",
-    width: "20%",
-    height: "100vh",
-  },
-  componentsListContainer: {},
-  componentsList: {
-    listStyle: "none",
-    borderTop: "1px solid gray",
-    borderBottom: "1px solid gray",
-    padding: "3px 5px",
-    color: "#4e4e4e",
-  },
-  mainWindow: {
-    width: "70%",
-    margin: "0 auto",
-  },
-};
+import appStyles from "./App.module.css";
 
 const App = () => {
   const [currentComponent, setCurrentComponent] = useState(componentsList[0]);
 
   return (
-    <div style={styles.mainContainer}>
-      <div style={styles.topNavContainer}>
+    <div className={appStyles.mainContainer}>
+      <div className={appStyles.topNavContainer}>
         <TopNavigationBar
           componentsList={componentsList}
           currentComponent={currentComponent}
           setCurrentComponent={setCurrentComponent}
         />
       </div>
-      <div style={styles.bottomContainer}>
-        <nav style={styles.sideNav}>
+      <div className={appStyles.bottomContainer}>
+        <nav className={appStyles.sideNav}>
           <SideNavigation
             componentsList={componentsList}
             currentComponent={currentComponent}
@@ -56,7 +26,7 @@ const App = () => {
             setCurrentComponent={setCurrentComponent}
           />
         </nav>
-        <div style={styles.mainWindow}>
+        <div className={appStyles.mainWindow}>
           <MainWindow currentComponent={currentComponent} />
         </div>
       </div>
