3b212874daac2151dddef3f4e459efb2b331ba4b
justfrankie <zhuangfrankie@gmail.com>
2024-01-22 17:04:31 -0800

    refactor in-line css objects into module folders

--- src/components/layout/TopNavigationBar.jsx
+++ src/components/layout/TopNavigationBar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import packageJson from "../../../package.json";
 import SearchBar from "../ui-components/SearchBar";
+import topNavStyles from "./TopNavigationBar.module.css"; // Import the CSS module
+
 const topListNav = ["Home", "Docs", "GitHub"];
 
 const TopNavigationBar = ({
@@ -10,19 +12,21 @@ const TopNavigationBar = ({
 }) => {
   const [searchQuery, setSearchQuery] = useState("");
   return (
-    <div style={styles.container}>
-      <div style={styles.leftSection}>
-        <div style={styles.logoContainer}>
+    <div className={topNavStyles.container}>
+      <div className={topNavStyles.leftSection}>
+        <div className={topNavStyles.logoContainer}>
           {/* Icon Placeholder */}
-          <span style={styles.iconPlaceholder}>📚</span>
+          <span className={topNavStyles.iconPlaceholder}>📚</span>
           {/* Hero Title */}
-          <span style={styles.heroTitle}>UI Storybook</span>
+          <span className={topNavStyles.heroTitle}>UI Storybook</span>
         </div>
         {/* Version Number */}
-        <span style={styles.versionNumber}>v{packageJson.version}</span>
+        <span className={topNavStyles.versionNumber}>
+          v{packageJson.version}
+        </span>
       </div>
 
-      <div style={styles.rightSection}>
+      <div className={topNavStyles.rightSection}>
         {/* Search Bar */}
         <SearchBar
           currentComponent={currentComponent}
@@ -32,9 +36,9 @@ const TopNavigationBar = ({
           componentsList={componentsList}
         />
         {/* GitHub Nav List Items */}
-        <ul style={styles.githubNav}>
+        <ul className={topNavStyles.githubNav}>
           {topListNav.map((item, index) => (
-            <li key={index} style={styles.githubNavItem}>
+            <li key={index} className={topNavStyles.githubNavItem}>
               {item}
             </li>
           ))}
@@ -42,54 +46,6 @@ const TopNavigationBar = ({
       </div>
     </div>
   );
-};
-
-// TODO convert this to modules styles
-const styles = {
-  container: {
-    backgroundColor: "#1a2b34",
-    color: "#fff",
-    padding: "10px",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  leftSection: {
-    display: "flex",
-    alignItems: "center",
-  },
-  logoContainer: {
-    display: "flex",
-    alignItems: "center",
-    marginRight: "20px",
-  },
-  iconPlaceholder: {
-    fontSize: "24px",
-    marginRight: "8px",
-  },
-  heroTitle: {
-    fontSize: "18px",
-    fontWeight: "bold",
-  },
-  versionNumber: {
-    fontSize: "12px",
-    color: "rgb(226, 81, 119)",
-  },
-  rightSection: {
-    display: "flex",
-    alignItems: "center",
-  },
-
-  githubNav: {
-    listStyle: "none",
-    padding: 0,
-    margin: 0,
-    display: "flex",
-  },
-  githubNavItem: {
-    margin: "0 8px",
-    cursor: "pointer",
-  },
 };
 
 export default TopNavigationBar;
--- src/components/ui-components/modules/InputForm/sub-components/InputList.jsx
+++ src/components/ui-components/modules/InputForm/sub-components/InputList.jsx
@@ -1,62 +1,21 @@
 import React from "react";
-
-// TODO convert this to modules styles
-
-const styles = {
-  ul: {
-    listStyle: "none",
-    padding: "0",
-    textAlign: "center",
-  },
-  li: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "white",
-    border: "1px solid #1a2b34",
-    borderRadius: "5px",
-    margin: "8px 0",
-    padding: "10px",
-  },
-  content: {
-    flex: "1",
-  },
-  h2: {
-    backgroundColor: "#f2f8fd",
-    margin: "0",
-    padding: "4px",
-  },
-  p: {
-    margin: "8px",
-  },
-  button: {
-    background: "transparent",
-    border: "0",
-    padding: "10px",
-    fontWeight: "bolder",
-    cursor: "pointer",
-    borderRadius: "5px",
-    marginLeft: "10px",
-    transition: "background-color 0.3s ease",
-  },
-  buttonHover: {
-    "&:hover": {
-      boxShadow: "0 0 1px black",
-    },
-  },
-};
+import inputListStyles from "./InputList.module.css";
 
 export default function InputList({ allTasks, handleDelete }) {
   return (
-    <ul style={styles.ul}>
+    <ul className={inputListStyles.ul}>
       {allTasks.map(({ title, description, id }) => (
-        <li key={id} style={styles.li}>
-          <div style={styles.content}>
-            <h2 style={styles.h2}>{title}</h2>
-            {!description ? null : <p style={styles.p}>{description}</p>}
+        <li key={id} className={inputListStyles.li}>
+          <div className={inputListStyles.content}>
+            <h2 className={inputListStyles.h2}>{title}</h2>
+            {!description ? null : (
+              <p className={inputListStyles.p}>{description}</p>
+            )}
           </div>
           <button
-            style={{ ...styles.button, ...(description && styles.buttonHover) }}
+            className={`${inputListStyles.button} ${
+              description && inputListStyles.buttonHover
+            }`}
             onClick={() => handleDelete(id)}
           >
             X
