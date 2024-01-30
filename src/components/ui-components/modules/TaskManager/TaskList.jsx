5aa607d22f610aac34ea3501248e0b229eaf3d1f
justfrankie <zhuangfrankie@gmail.com>
2024-01-29 23:57:56 -0800

    remove heading on task list

--- src/components/ui-components/modules/TaskManager/TaskList.jsx
+++ src/components/ui-components/modules/TaskManager/TaskList.jsx
@@ -4,7 +4,6 @@ import Button from "../../Button";
 const TaskList = ({ tasks, onRemove, onDone }) => {
   return (
     <div>
-      <h2>Tasks</h2>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
