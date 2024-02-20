6f8ecd1099678fecdc5eec62e8f267fdc93ad0c0
justfrankie <zhuangfrankie@gmail.com>
2024-02-20 01:34:43 -0800

    implement source code panel

--- src/components/layout/MainWindow.jsx
+++ src/components/layout/MainWindow.jsx
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MainWindow.module.css";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
+import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+SyntaxHighlighter.registerLanguage("jsx", jsx);
 
 const MainWindow = ({ currentComponent }) => {
-  const { id, name, Component } = currentComponent;
+  const { id, name, Component, rawJsx } = currentComponent;
+  const [sourceCodeOpen, setSourceCodeOpen] = useState(true);
+
+  const handleSourceCodeOpen = () => {
+    setSourceCodeOpen(!sourceCodeOpen);
+  };
 
   return (
     <div className={styles.container}>
       {id !== "home" && <h1>{name}</h1>}
-      <Component />
+      <div className={styles.componentWrapper}>
+        <div className={styles.componentContainer}>
+          <Component />
+        </div>
+        <div
+          className={`${styles.sourceCodeWrapper} ${
+            id === "home" && styles.hidden
+          }
+          `}
+        >
+          {id !== "home" && (
+            <FontAwesomeIcon
+              icon={faCode}
+              className={styles.sourceCodeButton}
+              onClick={handleSourceCodeOpen}
+              alt="Button to show Source Code"
+            />
+          )}
+          {rawJsx && sourceCodeOpen && (
+            <div className={styles.sourceCodeContainer}>
+              <div className={styles.sourceCode}>
+                <SyntaxHighlighter
+                  language="jsx"
+                  style={oneLight}
+                  customStyle={{
+                    backgroundColor: "inherit",
+                    fontSize: 13,
+                    overflowX: "auto",
+                    WebkitOverflowScrolling: "touch",
+                  }}
+                  wrapLongLines={true}
+                >
+                  {rawJsx}
+                </SyntaxHighlighter>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/componentsList.jsx
+++ src/componentsList.jsx
@@ -9,13 +9,29 @@ import GridWrapper from "./components/ui-components/modules/GridWrapper/GridWrap
 import StyledPanelDemo from "./components/ui-components/modules/StyledPanelDemo/StyledPanelDemo";
 
 const componentsList = [
-  { id: "home", name: "Home", Component: () => <Home /> },
-  { id: "button", name: "Button", Component: () => <ButtonList /> },
-  { id: "input-form", name: "Input Form", Component: () => <InputForm /> },
+  {
+    id: "home",
+    name: "Home",
+    Component: () => <Home />,
+    rawJsx: null,
+  },
+  {
+    id: "button",
+    name: "Button",
+    Component: () => <ButtonList />,
+    rawJsx: `<ButtonList />`,
+  },
+  {
+    id: "input-form",
+    name: "Input Form",
+    Component: () => <InputForm />,
+    rawJsx: `<InputForm />`,
+  },
   {
     id: "loading-spinner",
     name: "Loading Spinner",
     Component: () => <LoadingSpinnerList />,
+    rawJsx: `<LoadingSpinnerList />`,
   },
   {
     id: "search-bar",
@@ -29,21 +45,31 @@ const componentsList = [
         componentsList={[]}
       />
     ),
+    rawJsx: `<SearchBar
+        currentComponent={<div></div>}
+        setSearchQuery={() => null}
+        searchQuery={() => null}
+        setCurrentComponent={() => <div></div>}
+        componentsList={[]}
+      />`,
   },
   {
     id: "media-dashboard",
     name: "Media Dashboard",
     Component: () => <MediaDashboard />,
+    rawJsx: `<MediaDashboard />`,
   },
   {
     id: "grid-wrapper",
     name: "Responsive Grid Wrapper",
     Component: () => <GridWrapper />,
+    rawJsx: `<GridWrapper />`,
   },
   {
     id: "styled-panel",
     name: "Styled Panels",
     Component: () => <StyledPanelDemo />,
+    rawJsx: `<StyledPanelDemo />`,
   },
 ];
 
